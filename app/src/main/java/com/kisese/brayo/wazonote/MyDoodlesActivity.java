9c35ef94340915fcbbf72f114a7da35e3e6cbe25
kisese <brayokisese@gmail.com>
2015-12-30 02:26:16 +0300

    Support library upgrades, new nav drawer, icon, working on cleaning up UI and code

---
+++ app/src/main/java/com/kisese/brayo/wazonote/MyDoodlesActivity.java
@@ -0,0 +1,144 @@
+package com.kisese.brayo.wazonote;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.ActionBarActivity;
+import android.text.Html;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import java.io.File;
+
+/**
+ * Created by Brayo on 2/24/2015.
+ */
+public class MyDoodlesActivity extends ActionBarActivity {
+
+
+    // Declare variables
+    private String[] FilePathStrings;
+    private String[] FileNameStrings;
+    private File[] listFile;
+    GridView grid;
+    GridViewAdapter adapter;
+    File file;
+
+    private SharedPreferences notes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        notes = getSharedPreferences("doodle_status", Context.MODE_PRIVATE);
+        if(!notes.getString("doodles_empty", "").equals("no")){
+            setContentView(R.layout.empty_layout);
+        }else {
+            setContentView(R.layout.my_doodles);
+        }
+
+        try {
+            // Check for SD Card
+            if (!Environment.getExternalStorageState().equals(
+                    Environment.MEDIA_MOUNTED)) {
+                Toast.makeText(this, "Error! No SDCARD Found!", Toast.LENGTH_LONG)
+                        .show();
+            } else {
+                // Locate the image folder in your SD Card
+                file = new File("/sdcard/download/wazo_doodles/");
+                // Create a new folder if no folder named SDImageTutorial exist
+                file.mkdirs();
+            }
+
+            if (file.isDirectory()) {
+                listFile = file.listFiles();
+                // Create a String array for FilePathStrings
+                FilePathStrings = new String[listFile.length];
+                // Create a String array for FileNameStrings
+                FileNameStrings = new String[listFile.length];
+
+                for (int i = 0; i < listFile.length; i++) {
+                    // Get the path of the image file
+                    FilePathStrings[i] = listFile[i].getAbsolutePath();
+                    // Get the name image file
+                    FileNameStrings[i] = listFile[i].getName();
+                }
+            }
+
+
+            // Locate the GridView in gridview_main.xml
+            grid = (GridView) findViewById(R.id.gridview);
+            // Pass String arrays to LazyAdapter Class
+            adapter = new GridViewAdapter(this, FilePathStrings, FileNameStrings);
+            // Set the LazyAdapter to the GridView
+            grid.setAdapter(adapter);
+
+            // Capture gridview item click
+            grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view,
+                                        int position, long id) {
+
+                    Intent i = new Intent(MyDoodlesActivity.this, ViewImage.class);
+                    // Pass String arrays FilePathStrings
+                    i.putExtra("filepath", FilePathStrings);
+                    // Pass String arrays FileNameStrings
+                    i.putExtra("filename", FileNameStrings);
+                    // Pass click position
+                    i.putExtra("position", position);
+                    startActivity(i);
+                }
+
+            });
+        }catch (NullPointerException e){
+            Toast.makeText(this, "Error! No SDCARD Found!", Toast.LENGTH_LONG)
+                    .show();
+        }
+
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#F08080")));
+        getSupportActionBar().setTitle(Html.fromHtml("<font color=\"#FFFFFF\">My Doodles</font"));
+        getSupportActionBar().getThemedContext();
+        //getSupportActionBar().setIcon(new ColorDrawable(getResources().getColor(android.R.color.transparent)));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_add_new) {
+            Intent addDoodle = new Intent(this, Doodlz.class);
+            this.startActivity(addDoodle);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent i = new Intent(MyDoodlesActivity.this, MainMenuActivity.class);
+        startActivity(i);
+        super.onBackPressed();
+    }
+
+}
---
+++ app/src/main/java/storage/MyListAdapter.java
@@ -0,0 +1,52 @@
+package storage;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+
+import com.kisese.brayo.wazonote.R;
+
+/**
+ * Created by Brayo on 1/23/2015.
+ */
+public class MyListAdapter extends SimpleCursorAdapter {
+
+    private int lastPosition = -1;
+    Context context;
+    private static LayoutInflater inflater=null;
+    String[] from;
+    int position;
+    Cursor cursor;
+
+    public MyListAdapter(Context context, int layout, Cursor cursor, String[] from, int[] to) {
+        super(context, layout, cursor, from, to);
+        this.from = from;
+        this.cursor = cursor;
+        this.context = context;
+        position = from.length;
+        inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+        Animation animation = AnimationUtils.loadAnimation(context, (position > lastPosition) ? R.anim.up_from_bottom : R.anim.down_from_top);
+        view.startAnimation(animation);
+        lastPosition = position;
+        // Create the idno textview with background image
+        TextView idno = (TextView) view.findViewById(R.id.headeline);
+        idno.setText(cursor.getString(2));
+
+        // create the material textview
+        TextView materials = (TextView) view.findViewById(R.id.linksy);
+        materials.setText(cursor.getString(1));
+    }
+
+
+}
